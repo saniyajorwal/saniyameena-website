fe30f33e7e344b8a50517be75f8465839eec1a18
saniya <jeeprepsaniya@gmail.com>
2026-07-07 23:36:31 +0530

    Implement loading effect on enter button click
    Add loading effect and redirect on button click

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const enterBtn = document.getElementById("enterBtn");
 
+enterBtn.addEventListener("click", () => {
+
+    enterBtn.innerText = "LOADING...";
+
+    document.body.style.transition = "0.7s";
+
+    document.body.style.transform = "scale(1.2)";
+
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+
+        window.location.href = "menu.html";
+
+    },700);
+
+});
